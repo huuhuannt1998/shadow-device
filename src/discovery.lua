fb2580a43705582bb8648d3f70c8461c61305cac
Huan Bui <huuuhuannt1998@gmail.com>
2023-03-06 00:34:55 -0500

    update disco

--- src/discovery.lua
+++ src/discovery.lua
@@ -24,6 +24,84 @@ local ControlMessageBuilders = {
 
 local discovery = {}
 
+local function send_disco_request()
+  local listen_ip = "0.0.0.0"
+  local listen_port = 0
+  local multicast_ip = "239.255.255.250"
+  local multicast_port = 1900
+  local multicast_msg = table.concat(
+    {
+      'M-SEARCH * HTTP/1.1',
+      'HOST: 239.255.255.250:1900',
+      'MAN: "ssdp:discover"',
+      'MX: 4',
+      'ST: urn:Belkin:device:*',
+      '\r\n'
+    },
+    "\r\n"
+  )
+  local sock = assert(socket.udp(), "create discovery socket")
+  assert(sock:setsockname(listen_ip, listen_port), "disco| socket setsockname")
+  local timeouttime = socket.gettime() + 5 -- 5 second timeout, `MX` + 1 for network delay
+  assert(sock:sendto(multicast_msg, multicast_ip, multicast_port))
+  return sock, timeouttime
+end
+
+local function process_response(val)
+  local info = {}
+  val = string.gsub(val, "HTTP/1.1 200 OK\r\n", "", 1)
+  for k, v in string.gmatch(val, "([%g]+): ([%g ]*)\r\n") do
+    info[string.lower(k)] = v
+  end
+  return info
+end
+
+function Discovery.fetch_device_metadata(url)
+  -- Wemo responds with chunked encoding, must use ltn12 sink
+  local responsechunks = {}
+  local _, status, _ = http.request {
+    url = url,
+    sink = ltn12.sink.table(responsechunks),
+  }
+
+  local response = table.concat(responsechunks)
+
+  -- errors are coming back as literal string "[string "socket"]:1239: closed"
+  -- instead of just "closed", so do a `find` for the error
+  if string.find(status, "closed") then
+    log.debug("disco| ignoring unexpected socket close during metadata fetch, try parsing anyway")
+    -- this workaround is required because wemo doesn't send the required zero-length chunk
+    -- at the end of it `Text-Encoding: Chunked` HTTP message, it just closes the socket,
+    -- so ignore closed errors
+  elseif status ~= 200 then
+    log.error("disco| metadata request failed (" .. tostring(status) .. ")\n" .. response)
+    return nil, "request failed: " .. tostring(status)
+  end
+
+  local handler = xml_handler:new()
+  local xml_parser = xml2lua.parser(handler)
+  xml_parser:parse(response)
+
+  if not handler.root then
+    log.error("disco| unable to parse device metadata as xml")
+    return nil, "xml parse error"
+  end
+
+  local parsed_xml = handler.root
+
+  -- check if we parsed a <root> element
+  if not parsed_xml.root then
+    return nil
+  end
+
+  return {
+    name = tablefind(parsed_xml, "root.device.friendlyName"),
+    model = tablefind(parsed_xml, "root.device.modelName"),
+    mac = tablefind(parsed_xml, "root.device.macAddress"),
+    serial_num = tablefind(parsed_xml, "root.device.serialNumber"),
+  }
+end
+
 -- handle discovery events, normally you'd try to discover devices on your
 -- network in a loop until calling `should_continue()` returns false.
 function discovery.handle_discovery(driver, _should_continue)
